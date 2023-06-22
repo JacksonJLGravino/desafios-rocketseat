fc2029dad66a300f13fd52259258f56b79c0c44c
Jackson Gravino <jacksonjlgravino@gmail.com>
2023-06-22 16:35:16 -0300

    componente de upload

--- ComponenteDeUpload/main.js
+++
@@ -1,9 +0,0 @@
-const dropArea = document.querySelector(".drag-files")
-
-dropArea.addEventListener("dragover", () => {
-  dropArea.classList.add("dragover")
-})
-
-dropArea.addEventListener("dragleave", () => {
-  dropArea.classList.remove("dragover")
-})
